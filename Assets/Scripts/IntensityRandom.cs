5e6a319f1f10aba71e8ee6162b78f2015d353c05
Edison <ed_is_on@msn.com>
2019-05-30 13:50:24 +1000

    pushin for now

---
+++ Assets/Scripts/IntensityRandom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntensityRandom : MonoBehaviour
+{
+    Light light;
+    public float Timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = this.GetComponent<Light>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        if (Timer >9)
+        {
+            light.intensity = Random.Range(0.65f, 0.96f);
+            Timer = 0;
+        }
+        
+    }
+}
