8b9bfa821b6c8db1457da74f98f617b9228fd90d
AVataRR626 <matt.cabanag@gmail.com>
2019-06-01 15:38:56 +1000

    UI text fade progress
    poop

---
+++ Assets/Scripts/UITextFade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextFade : MonoBehaviour
+{
+    public float fadeRate = 0.2f;
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color c = text.color;
+        c.a = 0;
+        text.color = c;
+    }
+
+    private void Update()
+    {
+        Color c = text.color;
+
+        Debug.Log("C: a: " + c.a);
+        if(c.a < 1)
+        { 
+            c.a += fadeRate * Time.deltaTime;
+            text.color = c;
+        }
+    }
+}
