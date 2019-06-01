5e274f4dfe06bc4c3442988e9785b29ff5a322e8
AVataRR626 <matt.cabanag@gmail.com>
2019-06-01 19:48:31 +1000

    Text fade based on time
    OMFGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG too AAGGES lol

---
+++ Assets/Scripts/Text/FadeInTextLetterByLetter.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+// © 2017 TheFlyingKeyboard and released under MIT License
+// theflyingkeyboard.net
+// Modifed by Matt Cabanag
+public class FadeInTextLetterByLetter : MonoBehaviour
+{
+    [SerializeField] private Text textToUse;
+    [SerializeField] private bool useThisText = false;
+    [TextAreaAttribute(4, 15)]
+    [SerializeField] private string textToShow;
+    [SerializeField] private bool useTextText = false;
+    [SerializeField] private float fadeSpeedMultiplier = 0.25f;
+    [SerializeField] private bool fade;
+
+    private float colorFloat = 0.1f;
+    private int colorInt;
+    private int letterCounter = 0;
+    private string shownText;
+
+    public bool wordMode = false;
+    public string[] wordList;
+    int wordCounter = 0;
+    public float totalTime;   
+
+    private void Start()
+    {
+        if (useThisText)
+        {
+            textToUse = GetComponent<Text>();
+        }
+        if (useTextText)
+        {
+            textToShow = textToUse.text;
+        }
+
+
+        wordList = textToUse.text.Split(' ');        
+
+        if (fade)
+        {
+            Fade();
+        }
+
+        
+    }
+
+    private IEnumerator FadeInText()
+    {
+        while (letterCounter < textToShow.Length)
+        {
+
+            if (colorFloat < 1.0f)
+            {
+                colorFloat += Time.deltaTime * fadeSpeedMultiplier;
+                colorInt = (int)(Mathf.Lerp(0.0f, 1.0f, colorFloat) * 255.0f);
+                textToUse.text = shownText + "<color=\"#FFFFFF" + string.Format("{0:X}", colorInt) + "\">" + textToShow[letterCounter] + "</color>";
+            }
+            else
+            {
+                colorFloat = 0.1f;
+                shownText += textToShow[letterCounter];
+                letterCounter++;
+            }
+            
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeInWords()
+    {
+        while (wordCounter < wordList.Length)
+        {
+            if (colorFloat < 1.0f)
+            {
+                colorFloat += Time.deltaTime * fadeSpeedMultiplier;
+                colorInt = (int)(Mathf.Lerp(0.0f, 1.0f, colorFloat) * 255.0f);
+                textToUse.text = shownText + "<color=\"#FFFFFF" + string.Format("{0:X}", colorInt) + "\">" + wordList[wordCounter] + "</color>";
+            }
+            else
+            {
+                colorFloat = 0.1f;
+                shownText += wordList[wordCounter] + " ";
+                wordCounter++;
+            }
+            yield return null;
+        }
+    }
+    public void Fade()
+    {
+        if (wordMode)
+            StartCoroutine(FadeInWords());
+        else
+            StartCoroutine(FadeInText());
+    }
+}
---
+++ Assets/Scripts/Text/UITextFade.cs
@@ -0,0 +1,64 @@
+﻿//Matt's UI Text fading thingy...
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextFade : MonoBehaviour
+{
+    
+    public float fadeTime = 5;
+    public float fadeTimePerWord = 0.33f;
+    public float maxAlpha = 1;
+    public Text text;
+
+    public string[] wordList;
+
+    float colorFloat;
+    string shownText;
+    int wordIndex = 0;
+
+    public float framesInTime;
+    public float incrementValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wordList = text.text.Split(' ');
+        Debug.Log("Word length: " + wordList.Length);
+
+        //fadeIncrement = fadeTime/wordList.Length;
+
+        colorFloat = 0.1f;
+
+        fadeTimePerWord = fadeTime / wordList.Length;
+
+        framesInTime = fadeTimePerWord / Time.fixedDeltaTime;
+        incrementValue = maxAlpha / framesInTime;
+
+        Debug.Log("Fixed Delta Time: " + Time.fixedDeltaTime);
+        Debug.Log("Fixed updates in time: " + framesInTime);
+        Debug.Log("Increment value: " + incrementValue);
+    }
+
+    private void FixedUpdate()
+    {
+        if(wordIndex < wordList.Length)
+        { 
+            if(colorFloat < maxAlpha)
+            {   
+                colorFloat += incrementValue;
+
+                int colorInt = (int)(Mathf.Lerp(0.0f, 1.0f, colorFloat) * 255.0f);
+                text.text = shownText + "<color=\"#FFFFFF" + string.Format("{0:X}", colorInt) + "\">" + wordList[wordIndex] + "</color>";
+            }
+            else
+            {
+                colorFloat = 0.05f;
+                shownText += wordList[wordIndex] + " ";
+                wordIndex++;
+            }
+        }
+    }
+}
