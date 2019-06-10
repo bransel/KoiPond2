f262061d1b90460ca4c7f8140a5c70ede76b3fd5
Matt Cabanag <matt.cabanag@gmail.com>
2019-06-11 08:56:33 +1000

    parallel text fading maddafakas
    check out textfadelab scene

--- Assets/Scripts/Text/UITextFade.cs
+++ Assets/Scripts/Text/UITextFade.cs
@@ -8,15 +8,22 @@ using UnityEngine.UI;
 public class UITextFade : MonoBehaviour
 {
     
+    [Header("Basic Settings")]
     public float fadeTime = 5;
-    public float fadeTimePerWord = 0.33f;
-    public float randomElement = 0.5f;
+    public float fadeTimePerWord = 0.33f;    
     public float maxAlpha = 1;
     public Text text;
 
-    public string[] wordList;
+    [Header("Parallel Settings")]
+    public bool parallelMode = false;
+    public float randomElement = 0.5f;
 
-    float colorFloat;
+    [Header("System Variables")]
+    public string[] wordList;
+    public float[] colourFloats;
+    public float[] incrementValues;
+
+    float colourFloat;
     string shownText;
     int wordIndex = 0;
 
@@ -32,34 +39,78 @@ public class UITextFade : MonoBehaviour
 
         //fadeIncrement = fadeTime/wordList.Length;
 
-        colorFloat = 0.1f;
 
+
+        if (parallelMode)
+            ParallelInit();
+        else        
+            SerialInit();
+    }
+
+    void SerialInit()
+    {
         fadeTimePerWord = fadeTime / wordList.Length;
 
         framesInTime = fadeTimePerWord / Time.fixedDeltaTime;
         baseIncrementValue = maxAlpha / framesInTime;
-        incrementValue = baseIncrementValue + Random.Range(0,randomElement/ framesInTime);
+        incrementValue = baseIncrementValue;
+        colourFloat = 0.1f;
+    }
+    void ParallelInit()
+    {
+        colourFloats = new float[wordList.Length];
+        incrementValues = new float[wordList.Length];
 
-        Debug.Log("Fixed Delta Time: " + Time.fixedDeltaTime);
-        Debug.Log("Fixed updates in time: " + framesInTime);
-        Debug.Log("Increment value: " + baseIncrementValue);
+        framesInTime = fadeTime / Time.fixedDeltaTime;
+        baseIncrementValue = maxAlpha / framesInTime;
+        incrementValue = baseIncrementValue;
+
+        for (int i = 0; i < wordList.Length; i++)
+        {
+            colourFloats[i] = 0.08f;
+            incrementValues[i] = baseIncrementValue + (Random.Range(0, randomElement) * Time.fixedDeltaTime);
+        }
     }
 
     private void FixedUpdate()
     {
-        if(wordIndex < wordList.Length)
-        { 
-            if(colorFloat < maxAlpha)
-            {   
-                colorFloat += incrementValue;
+        if (parallelMode)
+            ParallelMode();
+        else
+            SequentialMode();
+    }
+    
+    void ParallelMode()
+    {
+        text.text = "";
 
-                int colorInt = (int)(Mathf.Lerp(0.0f, 1.0f, colorFloat) * 255.0f);
+        for (int i = 0; i < wordList.Length; i++)
+        {
+            if (colourFloats[i] < maxAlpha)
+            {
+                colourFloats[i] += incrementValues[i];
+            }
+
+            int colorInt = (int)(Mathf.Lerp(0.0f, 1.0f, colourFloats[i]) * 255.0f);
+            string wordString = "<color=\"#FFFFFF" + string.Format("{0:X}", colorInt) + "\">" + wordList[i] + "</color> ";
+            text.text += wordString;            
+        }
+    }
+
+    void SequentialMode()
+    {
+        if (wordIndex < wordList.Length)
+        {
+            if (colourFloat < maxAlpha)
+            {
+                colourFloat += incrementValue;
+                int colorInt = (int)(Mathf.Lerp(0.0f, 1.0f, colourFloat) * 255.0f);
                 text.text = shownText + "<color=\"#FFFFFF" + string.Format("{0:X}", colorInt) + "\">" + wordList[wordIndex] + "</color>";
             }
             else
             {
-                incrementValue = baseIncrementValue + Random.Range(0, randomElement / framesInTime);
-                colorFloat = 0.05f;
+                incrementValue = baseIncrementValue;// + Random.Range(0, randomElement / framesInTime);
+                colourFloat = 0.1f;
                 shownText += wordList[wordIndex] + " ";
                 wordIndex++;
             }
