55053ac79f4265d2f7bb0d329fb11468fd751397
CatDevPete <catdevpete@peter-liang.com>
2019-06-13 01:02:36 +1000

    Switching added to screensaver, removed default idle camera logic

--- Assets/Scripts/Screensaver.cs
+++ Assets/Scripts/Screensaver.cs
@@ -9,41 +9,37 @@ public class Screensaver : MonoBehaviour
     public GameObject primaryCamera;
     public GameObject secondaryCamera;
 
-    public float idleInterval = 5;    
-    public float idleClock;
     public float screenSwitchInterval = 60;
     public float screenSwitchClock = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        idleClock = idleInterval;
-    }
+
+    private Vector3 mouseDelta;
 
     // Update is called once per frame
     void Update()
     {
-        if(idleClock > 0)
+        if (screenSwitchClock > 0)
         {
-            idleClock -= Time.deltaTime;
-
-            primaryCamera.SetActive(true);
-            secondaryCamera.SetActive(false);
+            screenSwitchClock -= Time.deltaTime;
         }
         else
         {
-            if(screenSwitchClock > 0)
-            {
-                screenSwitchClock -= Time.deltaTime;
-            }
-            else
-            {
-                primaryCamera.SetActive(!primaryCamera.activeSelf);
-                secondaryCamera.SetActive(!primaryCamera.activeSelf);
-                screenSwitchClock = screenSwitchInterval;
-            }
+            primaryCamera.SetActive(!primaryCamera.activeSelf);
+            secondaryCamera.SetActive(!primaryCamera.activeSelf);
+            screenSwitchClock = screenSwitchInterval;
         }
 
         if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
-            idleClock = idleInterval;
+            screenSwitchClock = screenSwitchInterval;
+        
+        if (mouseDelta != Input.mousePosition)
+        {
+            screenSwitchClock = screenSwitchInterval;
+            mouseDelta = Input.mousePosition;
+        }
+    }
+
+    public void Switch()
+    {
+        screenSwitchClock = 0;
     }
 }
