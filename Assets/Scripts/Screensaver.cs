2725da14eb0247b2a68907e9f906f82dd6861bf4
Matt Cabanag <matt.cabanag@gmail.com>
2019-06-12 22:59:25 +1000

    extra ed shit for ss

--- Assets/Scripts/Screensaver.cs
+++ Assets/Scripts/Screensaver.cs
@@ -9,8 +9,10 @@ public class Screensaver : MonoBehaviour
     public GameObject primaryCamera;
     public GameObject secondaryCamera;
 
-    public float idleInterval = 5;
+    public float idleInterval = 5;    
     public float idleClock;
+    public float screenSwitchInterval = 60;
+    public float screenSwitchClock = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,8 +31,16 @@ public class Screensaver : MonoBehaviour
         }
         else
         {
-            primaryCamera.SetActive(false);
-            secondaryCamera.SetActive(true);
+            if(screenSwitchClock > 0)
+            {
+                screenSwitchClock -= Time.deltaTime;
+            }
+            else
+            {
+                primaryCamera.SetActive(!primaryCamera.activeSelf);
+                secondaryCamera.SetActive(!primaryCamera.activeSelf);
+                screenSwitchClock = screenSwitchInterval;
+            }
         }
 
         if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
